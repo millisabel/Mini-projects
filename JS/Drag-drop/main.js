b2957d485bb25657a4707cc69082ebf971179820
Helen <lionikia@gmail.com>
2020-12-13 21:17:11 +0200

    update project drag-n-drop

--- JS/Drag-drop/main.js
+++ JS/Drag-drop/main.js
@@ -1,39 +1,48 @@
 window.onload = function(){
+    setBallscolor('.balls__item');
     initHandle('.balls__item');
+};
+
+const setBallscolor = (imgClass) => {
+    let deg = 360*Math.random();
+    const img = document.querySelectorAll(imgClass);
+    for (let i = 0; i < img.length; i++) {
+        img[i].style = `-webkit-filter: hue-rotate(${deg}deg);filter: hue-rotate(${deg}deg);` ;
+        img[i].style = `filter: hue-rotate(${deg}deg);filter: hue-rotate(${deg}deg);` ;
+        deg += 30;
+    }
 };
 
 const initHandle = (imgClass) => {
     const img = document.querySelectorAll(imgClass);
+    const boxForTree = document.querySelector('.tree');
 
     for (let i = 0; i < img.length; i++) {
-        img[i].onmousedown = handleDown(event);
+        img[i].onmousedown = function(event){
+            img[i].ondragstart = function() {
+                return false;
+            };
+            img[i].style.position = 'absolute';
+            img[i].style.zIndex = 1000;
+            document.body.append(img[i]);
+            moveAt(event.pageX, event.pageY);
+            document.addEventListener('mousemove', onMouseMove);
+            img[i].onmouseup = function() {
+                document.removeEventListener('mousemove', onMouseMove);
+                img[i].onmouseup = null;
+                // console.log(img[i]);
+            };
+
+            function moveAt(pageX, pageY) {
+                img[i].style.left = pageX - img[i].offsetWidth / 2 + 'px';
+                img[i].style.top = pageY - img[i].offsetHeight / 2 + 'px';
+            }
+
+            function onMouseMove(event) {
+                moveAt(event.pageX, event.pageY);
+            }
+        };
     }
 };
-
-function handleDown(event){
-    console.log(event);
-    const item = this;
-    const e = event;
-    item.ondragstart = function() {
-        return false;
-    };
-    document.body.append(item);
-    moveAt(item);
-    document.addEventListener('mousemove', onMouseMove);
-    item.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        item.onmouseup = null;
-    };
-}
-
-function moveAt(item) {
-    console.log(item);
-    item.style.left = event.pageX - item.offsetWidth / 2 + 'px';
-    item.style.top = event.pageY - item.offsetHeight / 2 + 'px';
-}
-
-function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-}
 
 

9ad6008f8e7db67fa981873ff7cf50c555f62f11
millisabel <38396309+millisabel@users.noreply.github.com>
2021-01-03 15:49:34 +0200

    fixme: 'js/drag-n-drop'

--- JS/Drag-drop/main.js
+++ JS/Drag-drop/main.js
@@ -1,48 +1,83 @@
 window.onload = function(){
-    setBallscolor('.balls__item');
-    initHandle('.balls__item');
+    initBalls('.balls', 'img/ball.png', 544, 544, 30);
 };
 
-const setBallscolor = (imgClass) => {
+function initBalls(boxClass, imgSrc, imgWidth, imgHeight, imgNewWidth){
+    let parentForBalls = document.querySelector(boxClass);
+    let countBalls = Math.floor(parentForBalls.offsetWidth/imgNewWidth);
     let deg = 360*Math.random();
-    const img = document.querySelectorAll(imgClass);
-    for (let i = 0; i < img.length; i++) {
-        img[i].style = `-webkit-filter: hue-rotate(${deg}deg);filter: hue-rotate(${deg}deg);` ;
-        img[i].style = `filter: hue-rotate(${deg}deg);filter: hue-rotate(${deg}deg);` ;
+
+    for (let i = 0; i < countBalls; i++) {
+        createBalls(parentForBalls, imgSrc, imgWidth, imgHeight, deg);
         deg += 30;
     }
-};
+}
 
-const initHandle = (imgClass) => {
-    const img = document.querySelectorAll(imgClass);
-    const boxForTree = document.querySelector('.tree');
+function createBalls(parent, imgSrc, imgWidth, imgHeight, deg){
+    let ball = document.createElement('img');
 
-    for (let i = 0; i < img.length; i++) {
-        img[i].onmousedown = function(event){
-            img[i].ondragstart = function() {
-                return false;
-            };
-            img[i].style.position = 'absolute';
-            img[i].style.zIndex = 1000;
-            document.body.append(img[i]);
-            moveAt(event.pageX, event.pageY);
-            document.addEventListener('mousemove', onMouseMove);
-            img[i].onmouseup = function() {
-                document.removeEventListener('mousemove', onMouseMove);
-                img[i].onmouseup = null;
-                // console.log(img[i]);
-            };
+    ball.src = imgSrc;
+    ball.classList.add('balls__item');
+    ball.alt ='ball';
+    ball.width = imgWidth;
+    ball.height = imgHeight;
+    ball.style = `-webkit-filter: hue-rotate(${deg}deg);filter: hue-rotate(${deg}deg);` ;
+    ball.style = `filter: hue-rotate(${deg}deg);filter: hue-rotate(${deg}deg);` ;
+    parent.append(ball);
 
-            function moveAt(pageX, pageY) {
-                img[i].style.left = pageX - img[i].offsetWidth / 2 + 'px';
-                img[i].style.top = pageY - img[i].offsetHeight / 2 + 'px';
-            }
+    handleForBall(ball);
+}
 
-            function onMouseMove(event) {
-                moveAt(event.pageX, event.pageY);
-            }
+function handleForBall(ball){
+    const message = document.querySelector('.message');
+
+    ball.ondragstart = function() {
+        return false;
+    };
+
+    ball.onmousedown = () => {
+        let shiftX = event.clientX - ball.getBoundingClientRect().left;
+        let shiftY = event.clientY - ball.getBoundingClientRect().top;
+
+        ball.style.position = 'absolute';
+        ball.style.zIndex = 1000;
+        document.body.append(ball);
+
+        moveAt(event.pageX, event.pageY);
+
+        document.addEventListener('mousemove', onMouseMove);
+
+        ball.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            ball.onmouseup = null;
         };
-    }
-};
+
+        function onMouseMove(event) {
+            // const tree = document.querySelector('.tree>img');
+            moveAt(event.pageX, event.pageY);
+
+            // ball.hidden = true;
+            // let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+            // ball.hidden = false;
+            //
+            // if (!elemBelow) return;
+            //
+            // let droppableBelow = elemBelow.closest('.tree');
+            //
+            // if (droppableBelow) {
+            //     tree.style.background = 'red';
+            // }
+            // else{
+            //     tree.style.background = 'transparent';
+            // }
+        }
+
+        function moveAt(pageX, pageY) {
+            ball.style.left = pageX - shiftX + 'px';
+            ball.style.top = pageY - shiftY + 'px';
+        }
+    };
+}
+
 
 
